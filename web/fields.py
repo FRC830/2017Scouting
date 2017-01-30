040d11408e90b83482a69ff71725d7470c94c5de
830-R2 <R2@830.local>
2017-01-29 20:00:57 -0500

    almost organized

--- web/fields.py
+++ web/fields.py
@@ -2,23 +2,23 @@ import flask_wtf
 from widgets import * #http://wtforms.readthedocs.org/en/latest/fields.html
 
 class Form(flask_wtf.Form):
-    match_id = IntegerField('Match #', buttons=False)
-    team_id = IntegerField('Team #', buttons=False)
+    match_id = IntegerField('Match #')
+    team_id = IntegerField('Team #')
     #Form Fields
-    crossed_baseline = RadioField('Crossed Baseline',choices = [('Yes','Yes'),('No','No')], default='No')
-    auton_gears = IntegerField('Gears Installed #', buttons=False)
-    teleop_gears = IntegerField('Gears Installed #',buttons=False)
-    hanging = RadioField('Did it hang?',choices = [('Yes','Yes'),('No','No')], default='No')
+    crossed_baseline = CheckboxButtonField('Crossed Baseline?', col_md=2)
+    auton_gears = CheckboxButtonField('Gear Installed?', col_md=2)
+    teleop_gears = IntegerField('Gears Installed', default=0)
+    hanging = CheckboxButtonField('Did it hang?', col_md=2)
     
-    auton_highgoal = IntegerField('Fuel in High goal',buttons=False)
-    auton_lowgoal = IntegerField('Fuel in Low goal',buttons=False)
-    teleop_highgoal = IntegerField('Fuel in High goal',buttons=False)
-    teleop_lowgoal = IntegerField('Fuel in Low goal',buttons=False)
+    auton_highgoal = IntegerField('Fuel in High goal', default=0)
+    auton_lowgoal = IntegerField('Fuel in Low goal', default=0)
+    teleop_highgoal = IntegerField('Fuel in High goal', default=0)
+    teleop_lowgoal = IntegerField('Fuel in Low goal', default=0)
     
-    auton_techfouls = IntegerField('Techfouls #', buttons=False)
-    auton_fouls = IntegerField('Fouls #', buttons=False)
-    teleop_techfouls = IntegerField('Techfouls #',buttons=False)
-    teleop_fouls = IntegerField('Fouls #', buttons=False)
+    auton_techfouls = IntegerField('Techfouls', default=0)
+    auton_fouls = IntegerField('Fouls', default=0)
+    teleop_techfouls = IntegerField('Techfouls', default=0)
+    teleop_fouls = IntegerField('Fouls', default=0)
 
-    missed_stuff = RadioField('Did you miss any fuel scoring?',choices = [('Yes','Yes'),('No','No')], default='No')
+    missed_stuff = CheckboxButtonField('Did you fall asleep?', col_md=2)
     comments = TextAreaField('', col_md=12)
