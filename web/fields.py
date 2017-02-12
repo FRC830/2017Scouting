0c8b915bd698e80fbaadc7dbb33cbdaa73313831
Joey Bishop <JoooooooeyBishop@FRC.830>
2017-02-12 15:16:46 -0500

    organized and added pickup fields

--- web/fields.py
+++ web/fields.py
@@ -1,3 +1,4 @@
+
 import flask_wtf
 from widgets import * #http://wtforms.readthedocs.org/en/latest/fields.html
 
@@ -5,17 +6,19 @@ class Form(flask_wtf.Form):
     match_id = IntegerField('Match #')
     team_id = IntegerField('Team #')
     #Form Fields
-    crossed_baseline = CheckboxButtonField('Crossed Baseline?', col_md=2)
-    auton_gears = CheckboxButtonField('Gear Installed?', col_md=2)
+    crossed_baseline = CheckboxButtonField('Crossed baseline?', col_md=2)
+    auton_gears = CheckboxButtonField('Gear installed?', col_md=2)
     auton_techfouls = IntegerField('Techfouls', default=0)
     auton_fouls = IntegerField('Fouls', default=0)
-    auton_highgoal = IntegerField('Fuel in High goal', default=0)
-    auton_lowgoal = IntegerField('Fuel in Low goal', default=0)
+    auton_highgoal = IntegerField('Fuel in high goal', default=0)
+    auton_lowgoal = IntegerField('Fuel in low goal', default=0)
 
-    teleop_gears = IntegerField('Gears Installed', default=0)
-    teleop_highgoal = IntegerField('Fuel in High goal', default=0)
-    teleop_lowgoal = IntegerField('Fuel in Low goal', default=0)
+    teleop_gears = IntegerField('Gears installed', default=0)
+    pickup_gears = CheckboxButtonField('Can it pick up gears?', col_md=3)
+    teleop_highgoal = IntegerField('Fuel in high goal', default=0)
+    teleop_lowgoal = IntegerField('Fuel in low goal', default=0)
+    pickup_fuel = CheckboxButtonField('Can it pick up fuel?', col_md=3)
     teleop_techfouls = IntegerField('Techfouls', default=0)
     teleop_fouls = IntegerField('Fouls', default=0)
-    hanging = CheckboxButtonField('Did it hang?', col_md=4)
+    hanging = CheckboxButtonField('Did it hang?', col_md=3)
     comments = TextAreaField('', col_md=12)
